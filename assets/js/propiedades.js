eda3a8d274a8c4a814e1cf7b90dfa72f0977703a
camipsv <cami.psv@gmail.com>
2025-02-03 13:27:44 -0300

    Hago un js aparte para las propiedades para mas orden. Cambios pequeños en estilos y añado la cantidad de baños de cada propiedad

---
+++ assets/js/propiedades.js
@@ -0,0 +1,142 @@
+
+// PROPIEDADES EN VENTA
+
+export const propiedades_venta = [
+    {
+    nombre: "Casa moderna con piscina",
+    src: "assets/img/1.webp",
+    descripcion: "Hermosa casa con diseño moderno, amplio jardín y piscina.",
+    ubicacion: "Av. Los Robles 123, Santiago",
+    habitaciones: 4,
+    baños: 3,
+    costo: 35000000,
+    smoke: true,
+    pets: true
+    },
+    {
+    nombre: "Departamento céntrico con balcón",
+    src: "./assets/img/2.jpg",
+    descripcion: "Moderno departamento en el corazón de la ciudad.",
+    ubicacion: "Calle Alameda 567, Santiago",
+    habitaciones: 2,
+    baños: 2,
+    costo: 25000000,
+    smoke: true,
+    pets: false
+    },
+    {
+    nombre: "Casa rústica en la montaña",
+    src: "./assets/img/3.jpg",
+    descripcion: "Acogedora cabaña con chimenea y vista panorámica.",
+    ubicacion: "Camino Cordillera 789, Valparaíso",
+    habitaciones: 3,
+    baños: 2,
+    costo: 20000000,
+    smoke: true,
+    pets: true
+    },
+    {
+    nombre: "Penthouse con terraza privada",
+    src: "./assets/img/4.jpg",
+    descripcion: "Lujo y comodidad con vista panorámica.",
+    ubicacion: "Av. Kennedy 345, Las Condes",
+    habitaciones: 5,
+    baños: 3,
+    costo: 55000000,
+    smoke: true,
+    pets: false
+    },
+    {
+    nombre: "Casa familiar con jardín",
+    src: "./assets/img/5.jpg",
+    descripcion: "Espaciosa casa ideal para familias, con patio grande.",
+    ubicacion: "Los Álamos 678, La Florida",
+    habitaciones: 4,
+    baños: 2,
+    costo: 28000000,
+    smoke: true,
+    pets: true
+    },
+    {
+    nombre: "Loft minimalista",
+    src: "./assets/img/6.jpg",
+    descripcion: "Diseño minimalista con grandes ventanales.",
+    ubicacion: "Barrio Italia, Providencia",
+    habitaciones: 1,
+    baños: 1,
+    costo: 12000000,
+    smoke: true,
+    pets: true
+    }
+    ];
+    
+    
+    // PROPIEDADES ALQUILER
+    export const propiedades_alquiler = [
+    {
+    nombre: "Departamento con vista al mar",
+    src: "./assets/img/7.jpg",
+    descripcion: "Moderno departamento con balcón y vista al océano.",
+    ubicacion: "Anida Costanera 234, Viña del Mar",
+    habitaciones: 2,
+    baños: 2,
+    costo: 750000,
+    smoke: true,
+    pets: false
+    },
+    {
+    nombre: "Casa de campo",
+    src: "./assets/img/8.jpg",
+    descripcion: "Hermosa casa de campo con amplios espacios verdes.",
+    ubicacion: "Camino Rural 456, Curicó",
+    habitaciones: 3,
+    baños: 3,
+    costo: 550000,
+    smoke: true,
+    pets: true
+    },
+    {
+    nombre: "Estudio en barrio bohemio",
+    src: "./assets/img/9.jpg",
+    descripcion: "Pequeño y acogedor estudio en el centro cultural.",
+    ubicacion: "Calle Lastarria 567, Santiago",
+    habitaciones: 1,
+    baños: 1,
+    costo: 400000,
+    smoke: true,
+    pets: false
+    },
+    {
+    nombre: "Dúplex con jardín privado",
+    src: "./assets/img/10.webp",
+    descripcion: "Elegante dúplex con dos pisos y áreas verdes.",
+    ubicacion: "Av. Manquehue 789, Las Condes",
+    habitaciones: 3,
+    baños: 2,
+    costo: 320000,
+    smoke: true,
+    pets: false
+    },
+    {
+    nombre: "Habitación en departamento compartido",
+    src: "./assets/img/11.jpg",
+    descripcion: "Habitación amoblada en departamento con estudiantes.",
+    ubicacion: "Calle Irarrázaval 345, Ñuñoa",
+    habitaciones: 1,
+    baños: 1,
+    costo: 200000,
+    smoke: false,
+    pets: false
+    },
+    {
+    nombre: "Casa colonial en el centro",
+    src: "./assets/img/12.jpg",
+    descripcion: "Antigua casa colonial restaurada, en pleno casco histórico.",
+    ubicacion: "Calle Merced 678, Santiago Centro",
+    habitaciones: 4,
+    baños: 3,
+    costo: 450000,
+    smoke: true,
+    pets: false
+    }
+    ];
